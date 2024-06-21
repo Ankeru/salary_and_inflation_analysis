d6cb5d2c7db58e2056501f836dc817e9ca49aee4
Ankeru <tavonaar@gmail.com>
2024-06-21 14:48:03 +0300

    del file

--- import matplotlib.py
+++
@@ -1,6 +0,0 @@
-import matplotlib
-import pandas
-import numpy
-print('matplotlib: {}'.format(matplotlib.__version__))
-print('pandas: {}'.format(pandas.__version__))
-print('numpy: {}'.format(numpy.__version__))
